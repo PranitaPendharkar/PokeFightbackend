63b6ce9f67e4e11fdd53468cebaf7b9db6c46b7d
KateOblomova <kate.oblom@gmail.com>
2023-10-10 16:48:07 +0200

    incresed timeout

--- dbinit.js
+++ dbinit.js
@@ -1,14 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const connection = await mongoose.connect(
-    process.env.MONGO_URI,
-    options,
-    (err) => {
-      if (err) console.log(err);
-      else console.log("mongdb is connected");
-    }
-  );
+  const connection = await mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 30000,
+  });
   console.log(`MongoDB is connected to ${connection.connection.name}`);
 };
 
