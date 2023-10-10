f9dc252ff41f5b792438860d1637a21fbad2667c
KateOblomova <kate.oblom@gmail.com>
2023-10-10 16:36:05 +0200

    added a flag

--- dbinit.js
+++ dbinit.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const connection = await mongoose.connect(process.env.MONGO_URI);
+  const connection = await mongoose.connect(
+    process.env.MONGO_URI,
+    options,
+    (err) => {
+      if (err) console.log(err);
+      else console.log("mongdb is connected");
+    }
+  );
   console.log(`MongoDB is connected to ${connection.connection.name}`);
 };
 
